e569ae5852185f245548d33236238c139d3978c9
Meera Patel <patelmeeray@gmail.com>
2021-01-04 18:06:06 -0500

    add data/, delete data source file

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @title Antineoplastics Notes
+#' @description
+#' Notes on the `False Positive` status of RxNorm Antineoplastic ingredients and
+#' additional comments from OHDSI Oncologists.
+#' @format A data frame with 298 rows and 6 variables:
+#' \describe{
+#'   \item{\code{concept_id}}{double}
+#'   \item{\code{concept_name}}{character}
+#'   \item{\code{vocabulary_id}}{character}
+#'   \item{\code{concept_class_id}}{character}
+#'   \item{\code{false_positive}}{logical}
+#'   \item{\code{comment}}{character}
+#'}
+"antineoplastics"
+
